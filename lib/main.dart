3570790e51caae8e8253ba2ef42e41241e19c195
Marlon042 <127366345+Marlon042@users.noreply.github.com>
2025-06-15 12:19:20 -0600

    Update main.dart
    Splash Screen funcional
    
    y todo lo demás tambien.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:prueba/app/app_widget.dart';
 // Importación necesaria para el ThemeBloc
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prueba/features/settings/bloc/theme_bloc.dart';
+import 'package:prueba/core/widgets/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,5 +17,38 @@ void main() async {
     cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
   );
 
-  runApp(BlocProvider(create: (_) => ThemeBloc(), child: const AppWidget()));
+  runApp(
+    BlocProvider(
+      create: (_) => ThemeBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreenWrapper(),
+      ),
+    ),
+  );
+}
+
+class SplashScreenWrapper extends StatefulWidget {
+  const SplashScreenWrapper({super.key});
+
+  @override
+  State<SplashScreenWrapper> createState() => _SplashScreenWrapperState();
+}
+
+class _SplashScreenWrapperState extends State<SplashScreenWrapper> {
+  @override
+  void initState() {
+    super.initState();
+    // Navegar a AppWidget después de 2 segundos
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (_) => const AppWidget()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashScreen();
+  }
 }
